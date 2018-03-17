11e6fe52790a4ab8c656e995207ce8054d8e5c15
Dmytro Borchuk <dborchuk@remit.se>
2018-03-17 17:57:49 +0200

    Added `add to home screen`

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,34 +2,35 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Drawer from 'react-motion-drawer';
 import Notifications from '../Notifications/container';
-// import appleTouchIcon from '../../assets/favicon/apple-touch-icon.png';
-// import favicon32x32 from '../../assets/favicon/favicon-32x32.png';
-// import favicon16x16 from '../../assets/favicon/favicon-16x16.png';
-// import webmanifest from '../../assets/favicon/site.webmanifest';
-// import safariPinnedTab from '../../assets/favicon/safari-pinned-tab.svg';
 import manifest from '../../manifest.webmanifest';
+import icon from '../../Asset 1@2x-8.png';
 
 const AppWrapper = styled.div`
 `;
 
-// {/* <link rel="apple-touch-icon" sizes="144x144" href={appleTouchIcon} />
-// <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
-// <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
-// <link rel="manifest" href={webmanifest} />
-// <link rel="mask-icon" href={safariPinnedTab} color="#5bbad5" />
-// <meta name="msapplication-TileColor" content="#da532c" />
-// <meta name="theme-color" content="#ffffff" /> */}
 export default function App() {
   return (
     <AppWrapper>
       <Helmet>
         <link rel="manifest" href={manifest} />
+        <link rel="apple-touch-icon" sizes="144x144" href={icon} />
+        <link rel="icon" type="image/png" sizes="32x32" href={icon} />
+        <link rel="icon" type="image/png" sizes="16x16" href={icon} />
+        <link rel="apple-touch-startup-image" href="/launch.png" />
+        <meta name="apple-mobile-web-app-title" content="Be With Me" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="mask-icon" href={icon} color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Helmet>
       <Router>
         <div>
           <Route path="/notifications" component={Notifications} />
-          <Link to="/notifications">Notifications</Link>
+          <Drawer>
+            <Link to="/notifications">Notifications</Link>
+          </Drawer>
         </div>
       </Router>
     </AppWrapper>
