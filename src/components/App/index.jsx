2ae96dae8ce497873020bf66275ce0736fce3499
Dmytro Borchuk <dborchuk@remit.se>
2018-03-17 17:29:49 +0200

    added manifest

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Notifications from '../Notifications/container';
@@ -8,24 +8,24 @@ import Notifications from '../Notifications/container';
 // import favicon16x16 from '../../assets/favicon/favicon-16x16.png';
 // import webmanifest from '../../assets/favicon/site.webmanifest';
 // import safariPinnedTab from '../../assets/favicon/safari-pinned-tab.svg';
+import manifest from '../../manifest.webmanifest';
 
 const AppWrapper = styled.div`
 `;
 
-// <Helmet>
-//   {/* Favicon */}
-//   <link rel="apple-touch-icon" sizes="144x144" href={appleTouchIcon} />
-//   <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
-//   <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
-//   <link rel="manifest" href={webmanifest} />
-//   <link rel="mask-icon" href={safariPinnedTab} color="#5bbad5" />
-//   <meta name="msapplication-TileColor" content="#da532c" />
-//   <meta name="theme-color" content="#ffffff" />
-// </Helmet>
-
+// {/* <link rel="apple-touch-icon" sizes="144x144" href={appleTouchIcon} />
+// <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
+// <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
+// <link rel="manifest" href={webmanifest} />
+// <link rel="mask-icon" href={safariPinnedTab} color="#5bbad5" />
+// <meta name="msapplication-TileColor" content="#da532c" />
+// <meta name="theme-color" content="#ffffff" /> */}
 export default function App() {
   return (
     <AppWrapper>
+      <Helmet>
+        <link rel="manifest" href={manifest} />
+      </Helmet>
       <Router>
         <div>
           <Route path="/notifications" component={Notifications} />
